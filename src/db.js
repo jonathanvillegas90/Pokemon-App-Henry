f651e5f6153b98cd83c239ab756aad724aa3ad92
Jonathan Villegas <77998536+jonathanvillegas90@users.noreply.github.com>
2022-05-12 10:36:49 -0300

    cambio de db bien

--- src/db.js
+++ src/db.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-//const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-const { DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  DATABASE_URL,
-  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
