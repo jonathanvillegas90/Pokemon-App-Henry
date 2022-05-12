84acca4adb4c2b6d003a5ce06ec76c947ce52f3b
Jonathan Villegas <77998536+jonathanvillegas90@users.noreply.github.com>
2022-05-12 12:51:10 -0300

    se cambio todo el db

--- index.js
+++ index.js
@@ -17,14 +17,10 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
-var server_port = process.env.DB_PORT || 80;
+var server_port = process.env.DB_PORT || process.env.PORT || 80;
 var server_host = process.env.DB_HOST || "0.0.0.0";
-
-// Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(server_port, server_host, function () {
     console.log("Listening on port %d", server_port);
