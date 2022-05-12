66d9aced0be1d0bdb87520db22fcfddaa363d0c8
Jonathan Villegas <77998536+jonathanvillegas90@users.noreply.github.com>
2022-05-12 12:38:54 -0300

    cambios de json

--- src/db.js
+++ src/db.js
@@ -5,11 +5,11 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    ssl: false,
+    ssl: { rejectUnautorized: false },
   }
 );
 const basename = path.basename(__filename);
